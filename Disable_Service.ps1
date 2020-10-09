8b5248237621a1ce036eba1f15b626a66b60a94c
wanieldilson <68431297+wanieldilson@users.noreply.github.com>
2020-10-09 10:53:24 +0100

    Update and rename Untitled3.ps1 to Disable_Service.ps1

--- Disable_Service.ps1
+++ Disable_Service.ps1
@@ -1,7 +1,7 @@
-﻿## Script to disable any Service ##
-## D.Wilson October 2020         ##
+## Script to disable any Service ##
+## D.Wilson October 2020          ##
 
-$ServiceName = "XboxGipSvc"
+$ServiceName = "ServiceName"
 $Service = Get-Service -Name $ServiceName
 
 ## Checks if the service is running and disables it if it is ##
@@ -12,9 +12,12 @@ if ($Service.Status -eq "Running"){
 ## Variable for scheduled taks argument, triple quoted to wrap the whole thing in quotes ##
 $DisableService = """Set-Service -Name $Servicename  -Status Stopped -StartupType Disabled"""
 
+## Create new scheduled task ##
 $A = New-ScheduledTaskAction -Execute "powershell.exe" -Argument $DisableService
 $T = New-ScheduledTaskTrigger -AtLogOn
 $P = New-ScheduledTaskPrincipal -UserId "nt authority\localservice" -RunLevel Highest
 $S = New-ScheduledTaskSettingsSet 
 $task = New-ScheduledTask -Action $A -Principal $P -Trigger $T -Settings $S
-Register-ScheduledTask T6 -InputObject $task
+
+## Register the new task ##
+Register-ScheduledTask DisableKillerNetwork -InputObject $task
